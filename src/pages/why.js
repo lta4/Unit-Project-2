df6e4f5bfc2095e057fcf17f4b0cee3ac4709a84
Andrew <andrew.mk4@yahoo.com>
2021-01-18 01:24:31 -0800

    Response Update

--- src/pages/why.js
+++ src/pages/why.js
@@ -4,6 +4,8 @@ import Form from "../components/form"
 const Why = (props) => {
     return <div>
         <Form />
+        <h2>Coppyright-Star Wars and all associated names are copyright Lucasfilm ltd.</h2>
+        <h2>I'd like to thank swapi.tech for the api</h2>
     </div>
 }
 console.log()
